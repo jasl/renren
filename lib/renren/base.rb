b841304c058f1bae3d7c0e4deb15cef53865d009
David Sun <sunzheng.david@gmail.com>
2012-03-08 11:09:34 +0800

    Update

--- lib/renren/base.rb
+++ lib/renren/base.rb
@@ -33,27 +33,4 @@ module Renren
         params
       end
   end
-
-  class ByRefeshToken
-    attr_accessor :params
-
-    def initialize(refresh_token)
-      @refresh_token = refresh_token
-    end
-
-    def get_token
-      params = {}
-      params[:grant_type] = 'refresh_token'
-      params[:refresh_token] = @refresh_token
-      params[:client_id] = Config.api_key
-      params[:client_secret] = Config.api_secret
-      uri = URI.parse('http://graph.renren.com/oauth/token')
-      uri.query = URI.encode_www_form(params)
-      res = MultiJson.decode(Net::HTTP.get(uri))
-      if res['scope']
-        res['access_token']
-      else
-        nil
-      end
-  end
 end
